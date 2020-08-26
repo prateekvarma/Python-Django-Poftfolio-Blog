6693e320cb406075726abb89cf89acff45926d12
unknown <prtk.varma@gmail.com>
2020-08-26 01:50:24 -0600

    added urls and view for individual blog posts

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from . models import Blog
 def all_blogs(request):
     blogs = Blog.objects.all()
     return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    return render(request, 'blog/detail.html', {'id':blog_id})
