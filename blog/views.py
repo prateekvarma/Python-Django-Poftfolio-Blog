0582d98dbf49dc1dd4aae641f426669c3a0e868f
unknown <prtk.varma@gmail.com>
2020-09-08 19:35:52 -0600

    function to display each blog post separate

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Blog
 
 def all_blogs(request):
@@ -6,4 +6,5 @@ def all_blogs(request):
     return render(request, 'blog/all_blogs.html', {'blogs':blogs})
 
 def detail(request, blog_id):
-    return render(request, 'blog/detail.html', {'id':blog_id})
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':blog})
